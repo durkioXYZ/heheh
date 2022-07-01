f65b3881a247784e9fa1a09fae4128eecf791cea
sxui_the_g <95177117+wspitssxui@users.noreply.github.com>
2022-07-01 23:37:50 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports.options = {
+    token: 'OTg4MTIzMDY5NjY5MzE4NzU2.GV09Di.BJjV96fKSItgfsOXk0xvKfBS7an7QemaAieazU',
+    topicChannels: [
+      '（📊）סטטוס-שרת'
+    ],
+    roles: ["#"],
+    servers: [
+        { name: 'Server1', url: 'http://181.214.197.205:30120', alias: 'NewYork' },
+        { name: 'Server2', url: 'http://181.214.197.205:30121', alias: 'LosAngeles' }
+    ],
+    pollRate: 30,
+};
